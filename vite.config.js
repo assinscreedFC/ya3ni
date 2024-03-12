0ab817c1f2cef7c5f158bdb5b8ee877dd80cd56d
anis <anishammouche50@gmail.com>
2024-03-12 15:39:13 +0000

    go

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,18 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     rollupOptions: {
+      assetsDir: "assets",
       input: {
         main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
         forum: resolve(__dirname, "contact.html"),
         menu: resolve(__dirname, "menu.html"),
       },
+      output: {
+        // Conserve les noms de fichiers d'origine pour les ressources statiques
+        assetFileNames: "[name][extname]",
+      },
     },
+    base: "/ya3ni/",
   },
-  base: "/ya3ni/",
 });
