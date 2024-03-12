66c46635389b91bc28691dd2c5835c11e6c6cc9b
anis <anishammouche50@gmail.com>
2024-03-12 15:11:14 +0000

    Updates

---
+++ assets/forum-1EN24oix.js
@@ -0,0 +1,1 @@
+import{c as o}from"./foot-CSwEog69.js";import"https://kit.fontawesome.com/4c536a6bd5.js";o();
---
+++ assets/main-BQqe6csp.js
@@ -0,0 +1,1 @@
+import{c as t}from"./foot-CSwEog69.js";import"https://kit.fontawesome.com/4c536a6bd5.js";const o=document.querySelector(".button");o.addEventListener("click",()=>{window.location.href="./menu.html"});t();
